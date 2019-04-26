b9ba16b72c911014ce08b42f6686a71cef0bec49
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 15:01:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ def line(line)
   if line.length==0
     puts "The line is currently empty."
   else
-    printLine = []
+    printLine = ["The line is currently:"]
     printString = ""
-    printLine.push("The line is currently:")
     line.each_with_index do |n,p| printLine.push(" #{p+1}. #{n}")
     end
     printLine.each do |txt| printString << "#{txt}" end
